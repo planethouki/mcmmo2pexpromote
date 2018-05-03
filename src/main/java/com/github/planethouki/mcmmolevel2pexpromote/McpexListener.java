b60ab9b887b944619e3bdeb058c6c61cea954d27
planethouki <syn_tok@hotmail.co.jp>
2018-05-03 20:58:41 +0900

    message fix, pom id fix

--- src/main/java/com/github/planethouki/mcmmolevel2pexpromote/McpexListener.java
+++ src/main/java/com/github/planethouki/mcmmolevel2pexpromote/McpexListener.java
@@ -30,26 +30,27 @@ public class McpexListener implements Listener {
 			this.pexPromote(player);
 		}
 
-		plugin.getLogger().info("McMMOPlayerLevelUpEvent");
-		plugin.getLogger().info("LevelsGained: " + Integer.toString(event.getLevelsGained()));
-		plugin.getLogger().info("SkillLevel: " + Integer.toString(event.getSkillLevel()));
-		plugin.getLogger().info("Skill: " + event.getSkill().getName());
-		plugin.getLogger().info("PowerLevel: " + Integer.toString(mcPlayer.getPowerLevel()));
+//		plugin.getLogger().info("McMMOPlayerLevelUpEvent");
+//		plugin.getLogger().info("LevelsGained: " + Integer.toString(event.getLevelsGained()));
+//		plugin.getLogger().info("SkillLevel: " + Integer.toString(event.getSkillLevel()));
+//		plugin.getLogger().info("Skill: " + event.getSkill().getName());
+//		plugin.getLogger().info("PowerLevel: " + Integer.toString(mcPlayer.getPowerLevel()));
 	}
 
 	private void pexPromote(Player player) {
 		PermissionUser user = PermissionsEx.getUser(player);
 		if (user == null) {
 			plugin.getLogger().severe("Pex cannot find user. User=" + player.getName());
+			return;
 		}
 		user.getRank(null);
 		try {
 			user.promote(null, null);
 			player.sendMessage("Your role is level up! Now: " + user.getRankLadderGroup(null).getName());
-			plugin.getLogger().info("Promote User=" + user.getName() + " rank=" + user.getRank(null));
+			plugin.getLogger().info("Promote User=" + user.getName() + " Group=" + user.getRankLadderGroup(null).getName());
 		} catch (RankingException e) {
 			//e.printStackTrace();
-			plugin.getLogger().warning(e.getMessage() + " User=" + e.getTarget() + " Rank=" + user.getRank(null));
+			plugin.getLogger().warning(e.getMessage() + " User=" + e.getTarget() + " Group=" + user.getRankLadderGroup(null).getName());
 		}
 	}
 
